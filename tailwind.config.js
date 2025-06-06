a3c5d5b41cd3f6dbfb45015cbead4af34e2bb441
mumbla-uk <rossandrewjordan@icloud.com>
2025-06-06 02:50:52 +0100

    Hopefully changes to style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    // Ensure this path covers all your React components (.js, .jsx, .ts, .tsx)
-    "./src/**/*.{js,jsx,ts,tsx}",
-    // IMPORTANT: Also include your public HTML file, as it often contains the root element where
-    // some initial Tailwind classes (like min-h-screen, bg-gray-50) might be applied.
-    "./public/index.html",
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // <--- Make sure this line is correct
   ],
   theme: {
     extend: {},
